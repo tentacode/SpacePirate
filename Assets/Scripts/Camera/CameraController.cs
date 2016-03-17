1aa9c36e6b34a9cdc8b127153d36bf9bfc64e77d
tentacode <contact@gabrielpillet.com>
2016-03-17 14:32:38 +0100

    First commit

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    const int PIXEL_PER_UNIT = 16;
+
+    public int pixelScaling = 6;
+
+    protected Camera camera;
+
+	void Start()
+    {
+        camera = GetComponent<Camera>();
+        camera.orthographicSize = Screen.height * 0.5f / (pixelScaling * PIXEL_PER_UNIT);
+	}
+}
---
+++ Assets/Scripts/Characters/CharacterMover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMover : MonoBehaviour
+{
+    public float moveSpeed;
+    protected Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+	void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalAxis);
+
+        animator.SetFloat("horizontalAxis", horizontalAxis);
+
+        if (horizontalAxis != 0) {
+            this.MoveHorizontal(horizontalAxis);
+        }
+	}
+
+    void MoveHorizontal(float horizontalAxis)
+    {
+        float moveValue = horizontalAxis * moveSpeed * Time.deltaTime;
+        int aproximation = 16;
+
+        moveValue = Mathf.Round(moveValue * 1000);
+        moveValue = moveValue % 625;
+        moveValue = moveValue / 1000;
+
+        //moveValue = Mathf.Round(moveValue * aproximation) / aproximation;
+
+        this.transform.position += Vector3.right * moveValue;
+    }
+}
